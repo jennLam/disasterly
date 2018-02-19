5d3a0e0e909efd4a60f896af97e3eafc541bdc97
jennLam <jennaylam@gmail.com>
2018-02-19 13:08:49 +0000

    Moved js to separate file and added method in data.py.

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 class DisasterData():
@@ -8,4 +9,13 @@ class DisasterData():
         self.file_name = file_name
         self.df = pd.read_csv(file_name)
 
+        self.df["incidentBeginDate"] = pd.to_datetime(self.df["incidentBeginDate"], format="%Y/%m/%d")
+        self.df["incidentEndDate"] = pd.to_datetime(self.df["incidentEndDate"], format="%Y/%m/%d")
 
+    def get_categories(self, column_name):
+        """Create a list of unique categories from dataframe."""
+
+        items = self.df[column_name].unique()
+        items = np.array(items).tolist()
+
+        return items
---
+++ static/checkr.js
@@ -0,0 +1,6 @@
+var unitedStates = {lat: 37.09024, lng: -95.712891};
+
+var map = new google.maps.Map(document.getElementById("map"), {
+    center: unitedStates,
+    zoom: 5,
+});
